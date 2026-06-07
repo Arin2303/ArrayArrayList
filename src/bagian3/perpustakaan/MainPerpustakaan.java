544a5d35b32620b60fd569ceb2fe82aadd07fad3
Arin2303 <arinn2410010597@gmail.com>
2026-06-07 19:37:29 +0800

    Selesai Bagian 3: Multi-Class.

---
+++ src/bagian3/perpustakaan/MainPerpustakaan.java
@@ -0,0 +1,30 @@
+package bagian3.perpustakaan;
+
+public class MainPerpustakaan {
+    public static void main(String[] args) {
+        // Membuat objek pengelola
+        Perpustakaan perpus = new Perpustakaan();
+
+        // Membuat objek Buku lalu memasukkannya ke koleksi
+        perpus.tambahBuku(new Buku("Laskar Pelangi", "Andrea Hirata", 2005));
+        perpus.tambahBuku(new Buku("Bumi Manusia", "Pramoedya", 1980));
+        perpus.tambahBuku(new Buku("Negeri 5 Menara", "Ahmad Fuadi", 2009));
+
+        System.out.println();
+        perpus.cariPenulis("Andrea Hirata");
+
+        perpus.tampilkanKoleksi();
+
+        System.out.println();
+        perpus.pinjamBuku("Bumi Manusia");
+        perpus.pinjamBuku("Bumi Manusia"); // coba pinjam kedua kali
+
+        System.out.println();
+        perpus.kembalikanBuku("Bumi Manusia");
+        perpus.kembalikanBuku("Bumi Manusia"); // coba kembalikan kedua kali
+
+        System.out.println();
+        perpus.tampilkanKoleksi();
+        System.out.println("Buku tersedia: " + perpus.jumlahTersedia());
+    }
+}
